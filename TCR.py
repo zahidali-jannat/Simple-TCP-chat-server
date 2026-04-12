5c854d45469f1cb8dc0407bb66778beb8bf66e2f
zahid ali <za2206129@gmail.com>
2026-04-12 21:49:29 +0530

    Second Commit Admin control added

--- TCR.py
+++ TCR.py
@@ -11,8 +11,9 @@ server=socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 server.bind((host , port ))
 server.listen()                                  # server starts listening for new clients 
 lock = threading.Lock()
-                                                 # First here i am going to use two empty list here when ever if new will join the server we will
-                                                 # put that client in clients list and their nick name in nicknames list 
+admin_name="ZAHID"
+admin_client=None                                                 # First here i am going to use two empty list here when ever if new will join the server we will
+                                              # put that client in clients list and their nick name in nicknames list 
 
 clients=[]
 nicknames=[]
@@ -57,7 +58,7 @@ def handle(client):
 def receive():
     while True:
         client , address=server.accept()           # Accept client all the time get there address and client name 
-
+        global admin_client
         print(f"Connected with {str(address)}")
         
         client.send('NICK_NAME'.encode('ascii'))   # Particular client koh bheja hua message  
@@ -65,9 +66,22 @@ def receive():
         # nicknames.append(nickname)               # Decoded form mai naame liye daala andr direct 
         # clients.append(client)
         client_socket[client]=nickname
-        print(f"Nickaname of the client is {client_socket[client]}")
-        broadcast(f"{client_socket[client]} joined the chat ".encode('ascii'))
-        client.send("Connected to the server".encode('ascii'))
+        if(nickname==admin_name):
+            if admin_client is None:
+
+                admin_client=client
+                check_admin_added=True
+                broadcast(f"{nickname} Admin Joined the chat !!!!! ".encode('ascii'))
+                broadcast("Admin joined the chat Be caution before typing !!!!! ".encode('ascii'))
+            else:
+                 client.send("Admin already added".encode('ascii'))
+                 client.close()
+                 continue       
+        else:
+
+            print(f"Nickaname of the client is {client_socket[client]}")
+            broadcast(f"{client_socket[client]} joined the chat ".encode('ascii'))
+            client.send("Connected to the server".encode('ascii'))
 
         thread=threading.Thread(target=handle, args=(client, ))
         thread.start()
@@ -89,7 +103,7 @@ def kick_user(username):
                 print(f"{username} kicked successfully")
                 return 
         print("User not found")    
-def server_commands():
+def server_commands(): 
     while True:
         cmd=input()
         if cmd.startswith("kick"):
